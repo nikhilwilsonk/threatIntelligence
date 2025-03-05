5797f115f439db136a8dcdb5ea103731d857af72
nikhilss219 <nikhil.wilsonk@gmail.com>
2025-03-05 15:06:40 +0530

    added threat class

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+class Config:
+    load_dotenv()
+    VIRUSTOTAL_API_KEY = os.getenv('VIRUSTOTAL_API_KEY')
+    SHODAN_API_KEY = os.getenv('SHODAN_API_KEY')
+
+    MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+    DATABASE_NAME = 'threat_intelligence_db'
